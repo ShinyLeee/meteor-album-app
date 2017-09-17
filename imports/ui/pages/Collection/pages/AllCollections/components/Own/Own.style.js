8f46363d316c0449a81d12b41d7a03a1664d6382
Shiny Lee <lshinylee007@gmail.com>
2017-09-17 15:30:24 +0800

    fix conflict

--- imports/ui/pages/Collection/pages/AllCollections/components/Own/Own.style.js
+++ imports/ui/pages/Collection/pages/AllCollections/components/Own/Own.style.js
@@ -1,28 +1,15 @@
 import styled from 'styled-components';
 import Paper from 'material-ui/Paper';
 
-<<<<<<< HEAD
 const inlineStyles = {
   AddIcon: {
-=======
-export const inlineStyles = {
-  addIcon: {
->>>>>>> master
     width: '48px',
     height: '48px',
     color: '#676767',
   },
 };
 
-<<<<<<< HEAD
 const AddWrapper = styled(Paper)`
-=======
-export const Wrapper = styled.div`
-  padding-bottom: 30px;
-`;
-
-export const AddWrapper = styled(Paper)`
->>>>>>> master
   display: inline-block;
   width: calc(50% - 2px);
   max-width: 200px;
@@ -32,20 +19,17 @@ export const AddWrapper = styled(Paper)`
   cursor: pointer;
 `;
 
-export const AddSvgWrapper = styled.div`
+const AddSvgWrapper = styled.div`
   width: 48px;
   height: 48px;
   margin: 72px auto 0;
 `;
 
-export const AddMessage = styled.span`
+const AddMessage = styled.span`
   display: block;
   margin-top: 8px;
   color: #676767;
   text-align: center;
 `;
-<<<<<<< HEAD
 
 export { inlineStyles, AddWrapper, AddSvgWrapper, AddMessage };
-=======
->>>>>>> master
